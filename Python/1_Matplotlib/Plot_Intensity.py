3f6689b46db4a245e491bf9d631173f1774ff2c0
Antonino Savojardo <antonino.savojardo@gmail.com>
2017-11-28 13:41:11 +0000

    Add files via upload

--- Python/1_Matplotlib/Plot_Intensity.py
+++ Python/1_Matplotlib/Plot_Intensity.py
@@ -1,8 +1,10 @@
+''' Script to Plot and Fitt '''
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 color_x = ["blue","green","yellow","orange","red"]
-label_x = ["z1","z2","z3","z4","z5"]
+label_x = ["0 m","250 m","500 m","750 m","1000 m"]
 
 for j in range(0,5):
 
@@ -16,13 +18,13 @@ for j in range(0,5):
 #Plot
 
   #plt.clf() # Clear the figure
-  plt.title("Intensity",fontsize=16)
-  plt.xlabel("$t$",fontsize=16)
-  plt.ylabel("$|u|^2$",fontsize=16)
-  plt.xlim([-5,35])
+  #plt.title("Intensity",fontsize=16)
+  plt.xlabel("$t$ [ps]",fontsize=16)
+  plt.ylabel("$|u|^2$ [W]",fontsize=16)
+  plt.xlim([-1,11])
   #plt.xticks([0,90,180,270,360],["$0$","$\\frac{1}{2}\\pi$","$\\pi$","$\\frac{3}{2}\\pi$","$2$$\\pi$"])
-  plt.ylim([0.00001,11])
-  plt.yscale("log")
+  plt.ylim([0.000001,110])
+  #plt.yscale("log")
   #plt.yticks([-0.05,0.00,0.05,0.10,0.15,0.20,0.25],["-0.05","0.00","0.05","0.10","0.15","0.20","0.25"])
   plt.tick_params(labelsize=16,width=1)
 
@@ -30,6 +32,6 @@ for j in range(0,5):
   plt.legend(numpoints=1,loc="upper right",frameon=False,fontsize=16)
   plt.tight_layout()
 
-plt.savefig("Intensity.png")
-#plt.show()
+plt.savefig("Intensity_Linear.png")
+plt.show()
 
